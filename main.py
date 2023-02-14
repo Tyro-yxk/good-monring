5d69b216929e89dac273f690d90dab6538c0c60d
tyro-yxk <tyro_yxk@163.com>
2023-02-14 23:40:32 +0800

    更新 main.py
    由于原来的获取天气的接口失效，所以我修改了天气预报的接口，同时增加了pm2.5的数据

--- main.py
+++ main.py
@@ -18,11 +18,69 @@ app_secret = os.environ["APP_SECRET"]
 user_ids = os.environ["USER_ID"].split(";")
 template_id = os.environ["TEMPLATE_ID"]
 
+he_key = os.environ["HE_KEY"]
 
-def get_weather(city):
-    url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
+
+# def get_weather(city):
+#     url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
+#     res = requests.get(url).json()
+#     weather = res['data']['list'][0]
+#     return weather
+
+
+def get_weather(city, he_key):
+    city_url = "https://geoapi.qweather.com/v2/city/lookup?location={}&key={}".format(city, he_key)
+    city_res = requests.get(city_url).json()
+    if city_res["code"] != "200":
+        return None
+    wea_city = city_res["location"][0]
+    cityId = wea_city["id"]
+    url = "https://devapi.qweather.com/v7/weather/now?location={}&key={}".format(cityId, he_key)
     res = requests.get(url).json()
-    weather = res['data']['list'][0]
+    if res["code"] != "200":
+        print("天气获取失败")
+    else:
+        wea = res["now"]
+
+    url = "https://devapi.qweather.com/v7/weather/3d?location={}&key={}".format(cityId, he_key)
+    res = requests.get(url).json()
+    if res["code"] != "200":
+        print("温度最值获取失败")
+    else:
+        max_temp = res["daily"][0]["tempMax"] + u"\N{DEGREE SIGN}" + "C"
+        min_temp = res["daily"][0]["tempMin"] + u"\N{DEGREE SIGN}" + "C"
+
+    # 获取空气质量
+    url = "https://devapi.qweather.com/v7/air/now?location={}&key={}".format(cityId, he_key)
+    res = requests.get(url).json()
+    if res["code"] != "200":
+        print("空气质量获取失败")
+    else:
+        category = res["now"]["category"]
+        # pm2.5
+        pm2p5 = res["now"]["pm2p5"]
+    id = random.randint(1, 16)
+    url = "https://devapi.qweather.com/v7/indices/1d?location={}&key={}&type={}".format(cityId, he_key, id)
+    res = requests.get(url).json()
+    proposal = ""
+    if res["code"] != "200":
+        print("建议获取失败")
+    else:
+        proposal += res["daily"][0]["text"]
+
+    weather = {
+        "weather": wea["text"],
+        "temp": wea["temp"] + u"\N{DEGREE SIGN}" + "C",
+        "wind": wea["windDir"],
+        "humidity": wea["humidity"],
+        "airQuality": category,
+        "high": max_temp,
+        "low": min_temp,
+        "p2m5": pm2p5,
+        "province": wea_city["adm1"],  # 省
+        "city": wea_city["adm2"]
+    }
+
     return weather
 
 
@@ -70,7 +128,6 @@ def split_start():  # 多个倒计时通知
     if start_date is None:
         return None
     return start_date.split(";")
- 
 
 
 client = WeChatClient(app_id, app_secret)
@@ -83,7 +140,7 @@ for index, aim_city in enumerate(split_city()):
     ci_name = "city"
     if index != 0:
         ci_name = ci_name + "_%d" % index
-    weather = get_weather(aim_city)
+    weather = get_weather(aim_city, he_key)
     province = weather["province"]  # 省
     city = weather["city"]  # 市，县，州
     t = aim_city
@@ -127,6 +184,10 @@ for index, aim_city in enumerate(split_city()):
         "lo_%s" % ci_name: {  # 最低温度
             "value": math.floor(weather['low']),
             "color": get_random_color()
+        },
+        "pm_%s" % ci_name: {
+            "value": math.floor(weather["p2m5"]),
+            "color": get_random_color()
         }
     }
     data.update(da)
@@ -147,7 +208,6 @@ for index, aim_start in enumerate(split_start()):
         "value": get_count(aim_start),
         "color": get_random_color()
     }
-
 
 da = {
     "y_date": {  # 日期，阳历
@@ -177,22 +237,22 @@ data.update(da)
 # print("发送了" + str(count) + "条消息")
 
 if __name__ == '__main__':
-  try:
-    client = WeChatClient(app_id, app_secret)
-  except WeChatClientException as e:
-    print('微信获取 token 失败，请检查 APP_ID 和 APP_SECRET，或当日调用量是否已达到微信限制。')
-    exit(502)
 
-  wm = WeChatMessage(client)
-  count = 0
-  try:
-    for user_id in user_ids:
-      print('正在发送给 %s, 数据如下：%s' % (user_id, data))
-      res = wm.send_template(user_id, template_id, data)
-      count+=1
-  except WeChatClientException as e:
-    print('微信端返回错误：%s。错误代码：%d' % (e.errmsg, e.errcode))
-    exit(502)
+    try:
+        client = WeChatClient(app_id, app_secret)
+    except WeChatClientException as e:
+        print('微信获取 token 失败，请检查 APP_ID 和 APP_SECRET，或当日调用量是否已达到微信限制。')
+        exit(502)
 
-  print("发送了" + str(count) + "条消息")
+    wm = WeChatMessage(client)
+    count = 0
+    try:
+        for user_id in user_ids:
+            print('正在发送给 %s, 数据如下：%s' % (user_id, data))
+            res = wm.send_template(user_id, template_id, data)
+            count += 1
+    except WeChatClientException as e:
+        print('微信端返回错误：%s。错误代码：%d' % (e.errmsg, e.errcode))
+        exit(502)
 
+    print("发送了" + str(count) + "条消息")
